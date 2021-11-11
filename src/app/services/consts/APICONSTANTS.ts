881a970b49065c7634b14b725fa9e31db640baa0
muhdszakari6 <salimzakari6@gmail.com>
2021-11-12 00:31:11 +0100

    'bits'

--- src/app/services/consts/APICONSTANTS.ts
+++ src/app/services/consts/APICONSTANTS.ts
@@ -1,5 +1,5 @@
 export const APICONSTANTS = {
-    Token : "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",
+    Token : "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",
     organization: "Angular",
     pageSize: 100,
     base: 'https://api.github.com',
